245fc6a499b9dbb12d731be995ad08a56dae8f95
Undecember <ehdtjskaqhd@gmail.com>
2020-04-08 23:25:40 +0900

    Submit P18783-A2 (WA)

--- 18783.cpp
+++ 18783.cpp
@@ -3,33 +3,48 @@
 using namespace std;
 
 int _N, _M, _K;
+vector<pair<int, int>> coms;
 
-void repeat(vector<int> & ans, vector<int> func);
+void mul(vector<int> & ret, vector<int> func);
 
 int main(void)
 {
     scanf("%d%d%d", &_N, &_M, &_K);
 
-    vector<int> arr;
-    for (int i = 1; i <= _N; i++) arr.push_back(i);
+    vector<int> func(_N, 0), ans;
 
     for (int i = 0; i < _M; i++)
     {
         int a, b;
         scanf("%d%d", &a, &b);
-        --a; --b;
-        for (int j = 0; j < (b - a + 1) / 2; j++) swap(arr[a + j], arr[b - j]);
+        coms.push_back(pair<int, int>(a - 1, b));
     }
 
-    vector<int> ans(arr);
-    for (int i = 1; i < _K; i++) repeat(ans, arr);
+    for (int i = 1; i <= _N; i++)
+    {
+        ans.push_back(i);
 
-    for (int i = 0; i < _N; i++) printf("%d\n", ans[i]);
+        int ind = i - 1;
+        for (int j = 0; j < _M; j++)
+        {
+            if (coms[j].first <= ind && ind < coms[j].second) ind = coms[j].first + coms[j].second - ind - 1;
+        }
+        func[ind] = i;
+    }
+
+    while (_K)
+    {
+        if (_K % 2) mul(ans, func);
+        _K /= 2;
+        mul(func, func);
+    }
+
+    for (int i = 0; i <_N; i++) printf("%d\n", ans[i]);
     return 0;
 }
 
-void repeat(vector<int> & ans, vector<int> func)
+void mul(vector<int> & ret, vector<int> func)
 {
-    for (int i = 0; i < _N; i++) ans[i] += ans[func[i] - 1] % 10000 * 10000;
-    for (int i = 0; i < _N; i++) ans[i] /= 10000;
+    for (int i = 0; i < _N; i++) ret[i] += ret[func[i] - 1] % 10000 * 10000;
+    for (int i = 0; i < _N; i++) ret[i] /= 10000;
 }
